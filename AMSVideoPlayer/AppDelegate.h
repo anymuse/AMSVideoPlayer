23bfda1d2a9f9d21c19b2815879853f8bcfe8a87
anymuse <anymuse@gmail.com>
2016-05-14 16:55:43 +0800

    initial project version

---
+++ AMSVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMSVideoPlayer
+//
+//  Created by 朱琨 on 16/5/14.
+//  Copyright © 2016年 Talenton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMSVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMSVideoPlayer
+//
+//  Created by 朱琨 on 16/5/14.
+//  Copyright © 2016年 Talenton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
